ae4baea8414d24ba770ed75f34af6a47b83008f2
teddyzxcv <asfsdg>
2021-11-10 22:55:47 +0300

    ViewController has tableView, navigation controller is added

---
+++ nabelova_1PW5/Controllers/ArticleViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  nabelova_1PW5
+//
+//  Created by Наталья Белова on 10.11.2021.
+//
+
+import UIKit
+
+class ArticleViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        300
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: "TableViewCell", for: indexPath) as! TableViewCell
+           cell.textLabel?.text = "Привет!"
+           return cell
+    }
+    
+
+    let tableView = UITableView.init(frame: .zero, style: UITableView.Style.grouped)
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.white
+        self.view.addSubview(self.tableView)
+        self.tableView.register(TableViewCell.self, forCellReuseIdentifier: "TableViewCell")
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        updateLayout(with: self.view.frame.size)
+        
+    }
+    
+   
+    
+
+    private func updateLayout(with size: CGSize) {
+       self.tableView.frame = CGRect.init(origin: .zero, size: size)
+    }
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+       super.viewWillTransition(to: size, with: coordinator)
+       coordinator.animate(alongsideTransition: { (contex) in
+          self.updateLayout(with: size)
+       }, completion: nil)
+    }
+}
+
+class TableViewCell: UITableViewCell {
+
+}
